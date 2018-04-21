06931ae3d60060b281efdadb240948bd8d651437
xiepeiliang <xiepeiliang@outlook.com>
2018-04-21 14:27:11 +0800

    添加启动页

---
+++ DecisionHelper/app/src/main/java/com/example/xpl/decisionhelper/SplashActivity.java
@@ -0,0 +1,35 @@
+/*
+function：起始页，显示startbg1.5s后跳转到main
+author：xpl
+create tiem:2018.4.21
+ */
+
+package com.example.xpl.decisionhelper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    //延迟1.5s
+    private int TIME_DISPLAY = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, TIME_DISPLAY);
+    }
+}
